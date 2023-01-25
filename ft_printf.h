d028951be5e31110812b8abb46b0fdddb355c288
HugooVer <hugo.vercelloneg@gmail.com>
2023-01-26 00:19:51 +0100

    first test for split

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:00:53 by hvercell          #+#    #+#             */
-/*   Updated: 2023/01/25 21:53:10 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/01/25 23:36:45 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,12 @@
 
 int		ft_printf(const char *format, ...);
 
-size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
 char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
