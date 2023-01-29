c5860b2439f4f5f08868fa39c440b0e7819dadfd
HugooVer <hugo.vercelloneg@gmail.com>
2023-01-29 15:14:00 +0100

    main comented

--- maintest.c
+++ maintest.c
@@ -6,21 +6,21 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 23:55:16 by hvercell          #+#    #+#             */
-/*   Updated: 2023/01/28 18:42:03 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/01/29 15:13:43 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+// #include "ft_printf.h"
 
-int	main(void)
-{
-	int	c;
-	int	f;
-	int	ft_f;
+// int	main(void)
+// {
+// 	int	c;
+// 	int	f;
+// 	int	ft_f;
 
-	c = 0;
-	f = printf("%i\n", c);
-	ft_f = ft_printf("%i\n", c);
-	printf("Exepted:	%i	return:	%i\n", c, f);
-	ft_printf("Got:		%i	return:	%i\n", c, ft_f);
-}
+// 	c = 0;
+// 	f = printf("%i\n", c);
+// 	ft_f = ft_printf("%i\n", c);
+// 	printf("Exepted:	%i	return:	%i\n", c, f);
+// 	ft_printf("Got:		%i	return:	%i\n", c, ft_f);
+// }
